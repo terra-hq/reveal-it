cf77c8455a84c5d01b634265b2a054eb1b95f784
Alberto Tocados <152868250+albertotocados@users.noreply.github.com>
2024-01-30 18:01:09 +0100

    First commit

--- src/js/RevealIt.js
+++ src/js/RevealIt.js
@@ -4,23 +4,24 @@ gsap.registerPlugin(ScrollTrigger)
 // let bk = breakpoints.reduce((target, inner) => Object.assign(target, inner), {})
 // console.log(bk.mobile)
 
-// import { breakpoints, currentBreakpoint, getCurrentBreakpoint, updateBreakpointOnResize } from '@teamthunderfoot/breakpoints';
+import { breakpoints, currentBreakpoint, getCurrentBreakpoint, updateBreakpointOnResize } from '@teamthunderfoot/breakpoints';
 
 class RevealIt{
-    constructor(_payload = {}){
-        console.log('RevealIt',_payload)
+    constructor(_payload = {}) {
+        console.log('RevealIt', _payload);
+        this.element = _payload.element;
+        if (!this.element) {
+            console.warn("RevealIt: No element provided");
+        }
         this.tl = gsap.timeline({});
-        console.log(this)
-        this.init()
+        console.log(this);
+        this.init();
     }
-    init(){
 
-        this.tl.to(document.querySelector(".js--reveal-it"),{duration:1,opacity:0,yoyo:true,repeat:-1})
-        setTimeout(()=>{
-
-            this.getAnimation();
-        },1000)
+    init() {
+        this.tl.to(this.element, { duration: 1, opacity: 0, yoyo: true, repeat: -1 });        
     }
+
     getAnimation(){
        return this.tl;
     }
