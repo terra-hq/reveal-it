38f379a30c99b1ff00f5a082af5356bea1ac7ede
Alberto Tocados <152868250+albertotocados@users.noreply.github.com>
2024-02-14 17:55:54 +0100

    config in vite - rollup config deleted

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,48 @@
-import { resolve } from 'path'
-import path from 'path';
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 const root = resolve(__dirname, 'src');
 const outDir = resolve(__dirname, 'dist');
+const entryPath = resolve(__dirname, 'src/js/RevealIt.js');
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root,
-  plugins: [],
+  plugins: [], 
   build: {
-    outDir,
-    emptyOutDir: true,
+    lib: {
+      entry: entryPath,
+      name: 'RevealIt', 
+      formats: ['es', 'umd'], // Output formats
+      fileName: (format) => `reveal-it.${format}.js`, // File naming 
+    },
+    outDir, 
+    emptyOutDir: true, 
     rollupOptions: {
-      input: {
-        main: resolve(root, 'index.html'),
-      }
-    }
+      external: ['gsap'],
+      output: {
+        globals: {
+          gsap: 'gsap'
+        },
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@js': path.resolve(__dirname, './src/js'),
-      '@scss': path.resolve(__dirname, './src/scss'),
-      '@assets': path.resolve(__dirname, './src/assets'),
+      '@': resolve(__dirname, './src'),
+      '@js': resolve(__dirname, './src/js'),
+      '@scss': resolve(__dirname, './src/scss'),
+      '@assets': resolve(__dirname, './src/assets'),
     },
   },
-  css: { 
-    preprocessorOptions: { 
-      scss: { 
+  css: {
+    preprocessorOptions: {
+      scss: {
         additionalData: `
-          @import "./src/scss/framework/_vars/_vars.scss";
-          @import "./src/scss/framework/_mixins/_mixins.scss";
-          @import "./src/scss/framework/foundation/foundation.scss";
-        ` 
-      }  
-    }
-  }  
-})
+          @import "@/scss/framework/_vars/_vars.scss";
+          @import "@/scss/framework/_mixins/_mixins.scss";
+          @import "@/scss/framework/foundation/foundation.scss";
+        `,
+      },
+    },
+  },
+});
